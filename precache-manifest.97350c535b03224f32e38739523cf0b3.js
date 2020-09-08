550b0c2f6ceabdcad970defe94c3c2363ed4c13b
h4riharan <hariharanmh001@gmail.com>
2020-09-08 22:38:38 +0530

    Initial commit

---
+++ precache-manifest.97350c535b03224f32e38739523cf0b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b3eed2b11ea4be12515efcc8145765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0af19595386c5e28143",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0af19595386c5e28143",
+    "url": "/static/js/main.19e8ba1e.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
